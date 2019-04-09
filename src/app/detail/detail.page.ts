c55fdb1ec32828fc51577c85d29135b639693f2d
Joe <sheninter@gmail.com>
2019-04-09 18:02:45 +0800

    使用echarts绘制图形。

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as echarts from 'echarts';
+import { log } from 'util';
 
 @Component({
   selector: 'app-detail',
@@ -10,6 +12,36 @@ export class DetailPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ionViewDidEnter() {
+    const container = document.getElementById('chart');
+    this.makeChart(container);
+  }
+
+  makeChart(container) {
+    const ec = echarts as any;
+    console.log(ec.version);
+    const chart = ec.init(container);
+    chart.setOption({
+      series: {
+        type: 'pie',
+        data: [
+          {
+            name: 'A',
+            value: 10
+          },
+          {
+            name: 'B',
+            value: 20
+          },
+          {
+            name: 'C',
+            value: 30
+          }
+        ]
+      }
+    });
   }
 
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import * as echarts from 'echarts';
+import 'echarts-liquidfill';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +13,19 @@ export class HomePage implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  ionViewDidEnter() {
+    const ec = echarts as any;
+    const container = document.getElementById('container');
+
+    const chart = ec.init(container);
+    chart.setOption({
+      series: {
+        type: 'liquidFill',
+        data: [0.5, 0.4, 0.3, 0.2]
+      }
+    });
   }
 
   navigate() {
